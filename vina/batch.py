0317e8432d344d6ac3a1079bc4ed86be5652bc50
Luqman Zaceria <luqmanzaceria@gmail.com>
2022-02-28 17:51:09 -0800

    Completed vina batch submission script

--- vina/batch.py
+++ vina/batch.py
@@ -1,30 +1,20 @@
-from vina import Vina
 import os
+import subprocess
 
-v = Vina(sf_name='vina')
+vina_path = input("Path to Vina: ")
+
+conf_path = input("Path to configuration file: ")
 
 batch_path = input("Path to ligand directory: ")
-
-receptor = input("Receptor file name: ")
-v.set_receptor(receptor)
-
-custom_conf = input("Customize docking configuration? (y/n) ")
-if input == 'y':
-    v.compute_vina_maps(center=[center_x, center_y, center_z], box_size=[size_x, size_y, size_z])
-elif input == 'n':
-    print("Will perform blind docking.")
-else:
-    custom_conf
-    
-
 
 
 for ligand in os.listdir(batch_path):
     f = os.path.join(batch_path, ligand)
     # checking if it is a file
     if os.path.isfile(f):
-        v.set_ligand_from_file(ligand)
+        print("Docking " + ligand + "...")
+        ligand_path  = batch_path + "/" + ligand
+        log_name = (ligand.split('\\')[-1].split('.')[0]+ "_log.log")
         
-        # Dock the ligand
-        v.dock(exhaustiveness=32, n_poses=20)
-        v.write_poses('1iep_ligand_vina_out.pdbqt', n_poses=5, overwrite=True)
+        args = [vina_path, '--config', conf_path, '--ligand' , ligand_path, '--log', log_name]
+        subprocess.run(args)

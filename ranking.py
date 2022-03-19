dea9705843d58939519ab79225b9faf5a11cb04b
Luqman Zaceria <luqmanzaceria@gmail.com>
2022-03-19 13:05:23 -0700

    Fixed sorting of multiple ligands

--- ranking.py
+++ ranking.py
@@ -1,5 +1,8 @@
+import operator
+
+affinity_rankings = {}
+
 def rank_affinities(ligand, log_file):
-    affinity_rankings = {}
 
     affinity = []
     keep_phrases = ["-----"]
@@ -9,15 +12,19 @@ def rank_affinities(ligand, log_file):
 
     phrase_detected = False
 
-    for line in f:
-        if phrase_detected:
-            affinity.append(line)
-            phrase_detected = False
-        for phrase in keep_phrases:
-            if phrase in line:
-                phrase_detected = True
-                break
+    found_affinity = False
 
+    for line in f:
+        if not found_affinity:
+            if phrase_detected:
+                affinity.append(line)
+                found_affinity = True
+            for phrase in keep_phrases:
+                if phrase in line:
+                    phrase_detected = True
+                    break
+    
+    print(affinity)
     split_affinity = affinity[0].split(" ")
 
     while('' in split_affinity):
@@ -26,3 +33,5 @@ def rank_affinities(ligand, log_file):
     top_affinity = split_affinity[1]
     affinity_rankings[ligand]=(top_affinity)
     print(affinity_rankings)
+    sorted_affinity = dict(sorted(affinity_rankings.items(), key=operator.itemgetter(1),reverse=True))
+    print(sorted_affinity)
